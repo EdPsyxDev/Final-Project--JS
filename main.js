adc52609c001f0dd0515cd6aa888189eb2e1c0d2
edpsyxdev <edgarpersonalxd@gmail.com>
2025-03-27 14:06:58 -0500

    find your artist added

--- main.js
+++ main.js
@@ -11,3 +11,34 @@ button.addEventListener('click', () => {
     }, 2000);
   }
 });
+
+const bentoMenu = document.querySelector('.bento-menu');
+const closeBtn = document.querySelector('.close-btn');
+const showMenu = document.querySelector('.showMenu');
+
+let isMenuOpen = false;
+
+bentoMenu.addEventListener('click', () => {
+  if (isMenuOpen) return;
+
+  bentoMenu.classList.remove('show-anim-in');
+  bentoMenu.classList.add('hide-anim-out');
+
+  setTimeout(() => {
+    closeBtn.classList.add('show');
+    showMenu.classList.add('active');
+    isMenuOpen = true;
+  }, 100);
+});
+
+closeBtn.addEventListener('click', () => {
+  closeBtn.classList.remove('show');
+  showMenu.classList.remove('active');
+
+  setTimeout(() => {
+    bentoMenu.style.display = 'flex';
+    bentoMenu.classList.remove('hide-anim-out');
+    bentoMenu.classList.add('show-anim-in');
+    isMenuOpen = false;
+  }, 700);
+});

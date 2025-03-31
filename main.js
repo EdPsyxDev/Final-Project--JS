e848f0f59df7d11f217db06590e4490c013d8eb3
edpsyxdev <edgarpersonalxd@gmail.com>
2025-03-31 14:25:04 -0500

    .

--- main.js
+++ main.js
@@ -12,33 +12,37 @@ button.addEventListener('click', () => {
   }
 });
 
+
+
 const bentoMenu = document.querySelector('.bento-menu');
 const closeBtn = document.querySelector('.close-btn');
 const showMenu = document.querySelector('.showMenu');
 
-let isMenuOpen = false;
+if (bentoMenu && closeBtn && showMenu) {
+  let isMenuOpen = false;
 
-bentoMenu.addEventListener('click', () => {
-  if (isMenuOpen) return;
+  bentoMenu.addEventListener('click', () => {
+    if (isMenuOpen) return;
 
-  bentoMenu.classList.remove('show-anim-in');
-  bentoMenu.classList.add('hide-anim-out');
+    bentoMenu.classList.remove('show-anim-in');
+    bentoMenu.classList.add('hide-anim-out');
 
-  setTimeout(() => {
-    closeBtn.classList.add('show');
-    showMenu.classList.add('active');
-    isMenuOpen = true;
-  }, 100);
-});
+    setTimeout(() => {
+      closeBtn.classList.add('show');
+      showMenu.classList.add('active');
+      isMenuOpen = true;
+    }, 100);
+  });
 
-closeBtn.addEventListener('click', () => {
-  closeBtn.classList.remove('show');
-  showMenu.classList.remove('active');
+  closeBtn.addEventListener('click', () => {
+    closeBtn.classList.remove('show');
+    showMenu.classList.remove('active');
 
-  setTimeout(() => {
-    bentoMenu.style.display = 'flex';
-    bentoMenu.classList.remove('hide-anim-out');
-    bentoMenu.classList.add('show-anim-in');
-    isMenuOpen = false;
-  }, 700);
-});
+    setTimeout(() => {
+      bentoMenu.style.display = 'flex';
+      bentoMenu.classList.remove('hide-anim-out');
+      bentoMenu.classList.add('show-anim-in');
+      isMenuOpen = false;
+    }, 700);
+  });
+}

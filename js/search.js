ba2871c5222a10a255647d6afd18e2b45ae9d449
edpsyxdev <edgarpersonalxd@gmail.com>
2025-04-16 14:00:15 -0500

    .

--- js/search.js
+++ js/search.js
@@ -1,4 +1,36 @@
 
+const bentoMenu = document.querySelector('.bento-menu');
+const closeBtn = document.querySelector('.close-btn');
+const showMenu = document.querySelector('.showMenu');
+
+if (bentoMenu && closeBtn && showMenu) {
+  let isMenuOpen = false;
+
+  bentoMenu.addEventListener('click', () => {
+    if (isMenuOpen) return;
+
+    bentoMenu.classList.remove('show-anim-in');
+    bentoMenu.classList.add('hide-anim-out');
+
+    setTimeout(() => {
+      closeBtn.classList.add('show');
+      showMenu.classList.add('active');
+      isMenuOpen = true;
+    }, 100);
+  });
+
+  closeBtn.addEventListener('click', () => {
+    closeBtn.classList.remove('show');
+    showMenu.classList.remove('active');
+
+    setTimeout(() => {
+      bentoMenu.style.display = 'flex';
+      bentoMenu.classList.remove('hide-anim-out');
+      bentoMenu.classList.add('show-anim-in');
+      isMenuOpen = false;
+    }, 700);
+  });
+}
 
 // /---/ //
 
